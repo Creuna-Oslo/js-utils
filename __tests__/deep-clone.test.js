a024bfb37372fd9fa7fe57c3d4090253f928cdd7
Pelle Bjerkestrand <pelle@example.com>
2018-04-28 17:47:13 +0200

    cleans up tests.

--- __tests__/deep-clone.test.js
+++ __tests__/deep-clone.test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 
 import clone from '../deep-clone';
 
-test('deep-clone: object', t => {
+test('object', t => {
   const original = { a: 1, b: 2, c: { d: 3 } };
   const cloned = clone(original);
 
@@ -11,7 +11,7 @@ test('deep-clone: object', t => {
   t.not(cloned, original);
 });
 
-test('deep-clone: array', t => {
+test('array', t => {
   const original = [1, 2, 3, [4, 5]];
   const cloned = clone(original);
 
@@ -20,7 +20,6 @@ test('deep-clone: array', t => {
   t.not(cloned, original);
 });
 
-test('deep-clone: undefined', t => {
-  t.plan(1);
+test('undefined', t => {
   t.is(clone(undefined), undefined);
 });
