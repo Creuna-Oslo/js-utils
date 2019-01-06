a490f158438b23a21653940875417f77b81cd73a
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-01-06 16:25:44 +0100

    Refactors readme test to be sync

--- tests/readme.test.js
+++ tests/readme.test.js
@@ -4,36 +4,21 @@ import test from 'ava';
 
 import kebabToCamel from '../source/kebab-to-camel';
 
-test.cb('documents all modules', t => {
-  fs.readFile(
-    path.join(__dirname, '..', 'README.md'),
-    'utf8',
-    (err, readme) => {
-      if (!readme) {
-        t.fail("Can't find `README.md`");
-        t.end();
+test('documents all modules', t => {
+  const readme = fs.readFileSync(path.resolve(__dirname, '..', 'README.md'));
+
+  const sourceFiles = fs
+    .readdirSync(path.resolve(__dirname, '..', 'source'))
+    .filter(fileName => fileName.match(/\.js$/) && fileName !== 'index.js')
+    .map(fileName => kebabToCamel(fileName.replace('.js', '')))
+    .forEach(moduleName => {
+      if (!readme.includes(moduleName)) {
+        t.fail(`README does not include "${moduleName}".`);
         return;
       }
+    });
 
-      fs.readdir(path.join(__dirname, '..', 'source'), (err, files) => {
-        files
-          .filter(
-            fileName => fileName.match(/\.js$/) && fileName !== 'index.js'
-          )
-          .map(fileName => kebabToCamel(fileName.replace('.js', '')))
-          .forEach(moduleName => {
-            if (!readme.includes(moduleName)) {
-              t.fail(`README does not include "${moduleName}".`);
-              t.end();
-              return;
-            }
-          });
-
-        t.pass();
-        t.end();
-      });
-    }
-  );
+  t.pass();
 });
 
 test.cb('has no [TODO]s', t => {
